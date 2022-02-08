7bed664dc759082dd5d5e22c8decf0f0306ef5af
williamvelichko <williamvelichko2003@gmail.com>
2022-02-08 15:23:43 -0800

    im able to get the comment

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -19,7 +19,7 @@ router.get("/", (req, res) => {
 });
 
 router.get("/:id", async (req, res) => {
-  const { id } = req.params;
+  let { id } = req.params;
 
   try {
     Post.findById(id).then((post) => {
@@ -39,7 +39,7 @@ router.get("/:id", async (req, res) => {
 });
 
 router.post("/", (req, res) => {
-  const body = req.body;
+  let body = req.body;
 
   if (!body.title || !body.contents) {
     res
@@ -61,8 +61,8 @@ router.post("/", (req, res) => {
 });
 
 router.put("/:id", async (req, res) => {
-  const { id } = req.params;
-  const body = req.body;
+  let { id } = req.params;
+  let body = req.body;
 
   try {
     const findPost = Post.findById(id);
@@ -89,7 +89,7 @@ router.put("/:id", async (req, res) => {
 });
 
 router.delete("/:id", (req, res) => {
-  const { id } = req.params;
+  let { id } = req.params;
   Post.remove(id)
     .then((post) => {
       if (post == null) {
@@ -97,7 +97,7 @@ router.delete("/:id", (req, res) => {
           message: `The post with the specified ID ${id} does not exist`,
         });
       } else {
-        res.json(post);
+        res.status(200).json(post);
       }
     })
     .catch((err) => {
@@ -105,4 +105,24 @@ router.delete("/:id", (req, res) => {
       res.status(500).json({ message: "The post could not be removed" });
     });
 });
+
+router.get("/:id/comments", async (req, res) => {
+  try {
+    let { id } = req.params;
+    Post.findCommentById(id).then((comment) => {
+      if (comment == null) {
+        res.status(404).json({
+          message: `The post with the specified ID ${id} does not exist`,
+        });
+      } else {
+        res.json(comment);
+      }
+    });
+  } catch (e) {
+    res
+      .status(500)
+      .json({ message: "The comments information could not be retrieved" });
+  }
+});
+
 module.exports = router;
