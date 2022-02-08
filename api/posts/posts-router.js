af8d409b25e207d8138e9b799e868fb0ad2c1f82
williamvelichko <williamvelichko2003@gmail.com>
2022-02-08 11:55:37 -0800

    wip

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,12 @@
 // implement your posts router here
+const { Router } = require("express");
+
+const Post = require("./posts-model");
+
+const router = Router();
+
+router.get("/", (req, res) => {
+  console.log("router working");
+});
+
+module.exports = router;
